709acd6ecde59f54269711e33ba3f3bd055c916d
Celida Solis <celida.solis@outlook.com>
2023-10-30 18:33:47 -0400

    functionality

---
+++ script.js
@@ -0,0 +1,14 @@
+let html = '';
+let red;
+let green;
+let blue;
+let randomRGB;
+
+red = Math.floor(Math.random() * 256);
+green = Math.floor(Math.random() * 256);
+blue = Math.floor(Math.random() * 256);
+randomRGB = `rgb( ${red}, ${green}, ${blue} )`;
+html += `<div style="background-color: ${randomRGB}">1</div>`;
+
+
+document.querySelector('main').innerHTML = html;
